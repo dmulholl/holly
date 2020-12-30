632ee5bcbdd685480916b06bdbd25269c2f81c35
Darren Mulholland <dmulholl@tcd.ie>
2020-12-30 21:07:10 +0000

    Update template syntax for Ibis v3

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup
 
 setup(
     name = 'holly',
-    version = '0.2.1',
+    version = '0.3.0',
     py_modules = ['holly'],
     author = 'Darren Mulholland',
     url = 'https://github.com/dmulholl/holly',
